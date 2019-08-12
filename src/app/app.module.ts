8e20a0ee5e6d29aa7a6ab153f01b39f3c5f19385
Akhilesh <AkhileshM@outlook.in>
2019-08-12 18:50:28 +0530

    Created new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateSurveyTemplateComponent } from './create-survey-template/create-survey-template.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateSurveyTemplateComponent
   ],
   imports: [
     BrowserModule,
